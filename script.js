833a8409368d808a3a280f01b09588a11aa1358f
unknown <marose5388@gmail.com>
2021-06-01 18:30:07 -0500

    added hover states to icons and links

---
+++ script.js
@@ -0,0 +1,75 @@
+const imagesContainer = document.querySelector('.shop-page-content-container');
+const galleryImages = [
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+  {
+    src: './gallery-images/painting-1.jpeg',
+    alt: 'painting 1',
+    size: '5 X 10',
+    price: '$30',
+  },
+];
